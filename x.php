dd51388b7ffda833ca31ef9745ab227f82dc57c1
baek <you@example.com>
2024-10-16 07:27:44 +0000

    autoooo

--- x.php
+++ x.php
@@ -1,4 +1,4 @@
 <?php
-$output = shell_exec("sudo git config --global --add safe.directory /var/www/html/bo 2>&1 &&  sudo git add . 2>&1 && sudo git commit -m 'autoooo' 2>&1 && sudo git push origin main  2>&1");
+$output = shell_exec("sudo git config --global --add safe.directory /var/www/html/bo 2>&1 && sudo git pull --rebase --autostash origin main && sudo git add . 2>&1 && sudo git commit -m 'autoooo' 2>&1 && sudo git push origin main  2>&1");
 echo $output;
 ?>
